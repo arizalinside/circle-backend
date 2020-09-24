0e781e75efd03e09491001e9a382f6a4c6e39ff5
Arizal Sykes <arizalinc@gmail.com>
2020-09-25 01:25:19 +0700

    ✨ Setting up some dependencies

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const routerNavigation = require("./src");
+const socket = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const http = require("http");
+const server = http.createServer(app);
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Socket.io connect!");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.use("/", routerNavigation);
+server.listen(3000, () => {
+  console.log("Listening on Port 3000");
+});
