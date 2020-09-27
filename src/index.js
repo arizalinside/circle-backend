01f671adb51fdac4057c77d367993f08ddab4410
Arizal Sykes <arizalinc@gmail.com>
2020-09-27 19:41:36 +0700

    Create endpoint for friend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const route = require("express").Router();
 
-const user = require("./router/user");
+const user = require("./routes/user");
+const friend = require("./routes/friend");
 
 route.use("/user", user);
+route.use("/friend", friend);
 
 module.exports = route;
