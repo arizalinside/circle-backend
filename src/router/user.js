5d9d191125f73b3fc56c062c1742d5b768cbc71a
Arizal Sykes <arizalinc@gmail.com>
2020-10-23 20:35:03 +0700

    delete

--- src/router/user.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const { registerUser, loginUser, getUserById } = require("../controller/user");
-
-router.get("/:id", getUserById);
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-
-module.exports = router;
