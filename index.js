3996d1d9cc5840c5c833b7ccb10eadbc3d5b00cc
Arizal Sykes <arizalinc@gmail.com>
2020-11-05 20:38:55 +0700

    add cors

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const routerNavigation = require("./src");
 const socket = require("socket.io");
-
 const app = express();
-app.use(cors());
 
 const http = require("http");
 const server = http.createServer(app);
@@ -16,11 +14,27 @@ io.on("connection", (socket) => {
   console.log("Socket.io connect!");
 });
 
+app.use(cors());
+app.use(express.static("uploads"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-request-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.use("/", routerNavigation);
+
+app.get("*", (request, respond) => {
+  respond.status(404).send("Path Not Found !");
+});
+
 server.listen(3000, () => {
   console.log("Listening on Port 3000");
 });
