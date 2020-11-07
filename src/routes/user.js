c26b548c3ac52c2bff2ecc52effe32ab8bc82018
Arizal Sykes <arizalinc@gmail.com>
2020-11-07 19:18:37 +0700

    add patch and get status, patch location endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,14 +6,20 @@ const {
   getAllUser,
   getUserById,
   patchUserById,
+  patchStatus,
+  getUserStatus,
+  patchLocation,
 } = require("../controller/user");
 const { authorization } = require("../middleware/auth");
 const upload = require("../middleware/multer");
 
 router.get("/", authorization, getAllUser);
 router.get("/:id", authorization, getUserById);
+router.get("/status/:id", authorization, getUserStatus);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.patch("/update/:id", authorization, upload, patchUserById);
+router.patch("/update-status/:id", authorization, patchStatus);
+router.patch("/update-location/:id", authorization, patchLocation);
 
 module.exports = router;
