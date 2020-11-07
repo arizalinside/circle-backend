0ad1df6a7e15ba785aa2666fbfcdb4d2069c91f8
Arizal Sykes <arizalinc@gmail.com>
2020-11-07 19:15:10 +0700

    add chat endpoint

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const route = require("express").Router();
 
 const user = require("./routes/user");
 const friend = require("./routes/friend");
+const chat = require("./routes/chat");
 
 route.use("/user", user);
 route.use("/friend", friend);
+route.use("/chat", chat);
 
 module.exports = route;
